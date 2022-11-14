86f435852a016bd08896fa73211d8402147aa729
Zachary Tata <86603180+ztata@users.noreply.github.com>
2022-11-14 16:14:21 -0500

    package updates

--- BudgetAppAngular/src/app/app.module.ts
+++ BudgetAppAngular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+//import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatNativeDateModule} from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,7 +23,7 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     FormsModule,    
     ReactiveFormsModule,
-    MatDatepickerModule,
+    //MatDatepickerModule,
     MatInputModule,
     MatNativeDateModule,
     BrowserAnimationsModule,
